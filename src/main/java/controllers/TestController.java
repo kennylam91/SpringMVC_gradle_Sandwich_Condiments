276aac60f104a171749c17aeaab509cfea228325
kennylam91 <phamvanlam1991@gmail.com>
2019-06-28 15:22:11 +0700

    first commit

---
+++ src/main/java/controllers/TestController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+    @GetMapping
+    public ModelAndView test(){
+        ModelAndView modelAndView=new ModelAndView("test");
+        return modelAndView;
+    }
+}
